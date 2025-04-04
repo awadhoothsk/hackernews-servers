67bd61dc923d2dffdb5357b4a3e7537e0015199f
awadhootsk <awadhooths16@gmail.com>
2025-04-04 11:19:41 +0530

    implement user, post, comment, and like controllers and routes; add token middleware and error handling

---
+++ src/routes/routes.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { authenticationRoutes } from "./authentication-routes.js";
+import { usersRoutes } from "./user-routes.js";
+import { postsRoutes } from "./post-routes.js";
+import { likesRoutes } from "./like-routes.js";
+import { commentsRoutes } from "./comments-routes.js";
+import { logger } from "hono/logger";
+
+export const allRoutes = new Hono();
+
+allRoutes.use(logger());
+
+allRoutes.route("/auth", authenticationRoutes);
+allRoutes.route("/users", usersRoutes);
+allRoutes.route("/posts", postsRoutes);
+allRoutes.route("/likes", likesRoutes);
+allRoutes.route("/comments", commentsRoutes);
+
+allRoutes.get("/health", (context) => {
+  return context.json({ message: "All Ok" }, 200);
+});
